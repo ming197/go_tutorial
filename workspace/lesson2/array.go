87b0d3496cc54df31ed7370949443709935f3292
hmmei <ming197@qq.com>
2024-06-11 23:48:16 +0800

    declare and define an array

--- workspace/lesson2/array.go
+++ workspace/lesson2/array.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 func main() {
-    a := [5]int{}
+    // 1. define an array with 5 elements, all elements are 0
+    // a := [5]int{}
+    // 2. var a [5]int
+    var a [5]int
     a[0] = 1
     fmt.Println(a)
 }
